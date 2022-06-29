bf0027aae07f3db773a9f004d7c9acccaa9dd5a0
huy-raw <ruacon130@gmail.com>
2022-06-29 20:21:03 +0700

    feat: using respone entity and fix swagger

--- src/main/java/com/huyraw/demo/book/BookController.java
+++ src/main/java/com/huyraw/demo/book/BookController.java
@@ -1,11 +1,14 @@
 package com.huyraw.demo.book;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -17,21 +20,21 @@ import java.util.List;
 public class BookController {
 
 
-    private final  BookService bookService;
+    private final BookService bookService;
 
     @Autowired
-    public BookController(BookService bookService){
+    public BookController(BookService bookService) {
         this.bookService = bookService;
     }
 
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Found the book",
-                    content = { @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = Book.class)) }),
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class))}),
             @ApiResponse(responseCode = "400", description = "Invalid id supplied",
                     content = @Content),
             @ApiResponse(responseCode = "404", description = "Book not found",
-                    content = @Content) })
+                    content = @Content)})
     @GetMapping(value = "/")
     public List<Book> getBooks() {
         return bookService.getAllBooks();
@@ -39,29 +42,35 @@ public class BookController {
 
 
     @GetMapping(path = "/{id}")
-    public Book getBookById(@PathVariable("id")  Long id) {
-        return bookService.getBookById(id);
+
+    public ResponseEntity<Book> getBookById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(bookService.getBookById(id), HttpStatus.OK);
     }
 
 
     @PostMapping(value = "/")
-    @ResponseStatus
-    public Book importNewBook(@RequestBody Book book){
-        return bookService.addNewBook(book);
-
+    @ResponseStatus()
+    public ResponseEntity<Book> importNewBook(@RequestBody Book book) {
+        Book book_ = bookService.addNewBook(book);
+        if (book_ == null) {
+            return new ResponseEntity<>(book_, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(book_, HttpStatus.CREATED);
     }
 
     @DeleteMapping(path = "/{id}")
-    @ResponseStatus
-    public void deleteBook(@PathVariable("id") Long id){
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteBook(@PathVariable("id") Long id) {
+
         bookService.deleteBook(id);
     }
 
     @PutMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public void updateBook(
-        @PathVariable("id") Long id,
-        @RequestBody(required = false) Book book
-    ){
+            @PathVariable("id") Long id,
+            @RequestBody(required = false) Book book
+    ) {
         bookService.updateBook(id, book);
     }
 }
